fa924c00386f83e4187f0533cee96c9e351355e2
wsw835 <46206694+wsw835@users.noreply.github.com>
2022-08-08 16:02:07 +0800

    Update AddressConverter.java

--- src/main/java/com/wsw/converter/AddressConverter.java
+++ src/main/java/com/wsw/converter/AddressConverter.java
@@ -1,13 +1,3 @@
-/*
- * @(#) AddressConverter
- * 版权声明 厦门畅享信息技术有限公司, 版权所有 违者必究
- *
- * <br> Copyright:  Copyright (c) 2019
- * <br> Company:厦门畅享信息技术有限公司
- * <br> @author 温思伟
- * <br> 2019-09-23 20:43:11
- */
-
 package com.wsw.converter;
 
 import com.wsw.entity.db.Address;
