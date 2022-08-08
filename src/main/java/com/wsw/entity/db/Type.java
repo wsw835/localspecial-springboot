7bb4df76928a3f0bb9ea46b75b23546de6a2e565
wsw835 <46206694+wsw835@users.noreply.github.com>
2022-08-08 16:04:37 +0800

    Update Type.java

--- src/main/java/com/wsw/entity/db/Type.java
+++ src/main/java/com/wsw/entity/db/Type.java
@@ -1,12 +1,3 @@
-/*
- * @(#) Type
- * 版权声明 厦门畅享信息技术有限公司, 版权所有 违者必究
- *
- * <br> Copyright:  Copyright (c) 2019
- * <br> Company:厦门畅享信息技术有限公司
- * <br> @author 温思伟
- * <br> 2019-11-23 10:45:13
- */
 
 package com.wsw.entity.db;
 
