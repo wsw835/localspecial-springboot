60769826514b3ccb1ded4f0a0ddc8edcbd87f266
wsw835 <46206694+wsw835@users.noreply.github.com>
2022-08-08 16:05:13 +0800

    Update CommentsVO.java

--- src/main/java/com/wsw/entity/vo/CommentsVO.java
+++ src/main/java/com/wsw/entity/vo/CommentsVO.java
@@ -1,12 +1,3 @@
-/*
- * @(#) CommentsVO
- * 版权声明 厦门畅享信息技术有限公司, 版权所有 违者必究
- *
- * <br> Copyright:  Copyright (c) 2019
- * <br> Company:厦门畅享信息技术有限公司
- * <br> @author 温思伟
- * <br> 2019-11-23 10:44:57
- */
 
 package com.wsw.entity.vo;
 
