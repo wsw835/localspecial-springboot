03ab89819655228a7a177b11c36bcdec317ce1ef
wsw835 <46206694+wsw835@users.noreply.github.com>
2022-08-08 16:05:49 +0800

    Update PositionVO.java

--- src/main/java/com/wsw/entity/vo/PositionVO.java
+++ src/main/java/com/wsw/entity/vo/PositionVO.java
@@ -1,13 +1,3 @@
-/*
- * @(#) Position
- * 版权声明 厦门畅享信息技术有限公司, 版权所有 违者必究
- *
- * <br> Copyright:  Copyright (c) 2020
- * <br> Company:厦门畅享信息技术有限公司
- * <br> @author 温思伟
- * <br> 2020-05-15 11:54:09
- */
-
 package com.wsw.entity.vo;
 
 public class PositionVO {
